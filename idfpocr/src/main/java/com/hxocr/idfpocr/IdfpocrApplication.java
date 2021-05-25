a06c6155b15c82fa2ae96cceea85be8bbbaefc7d
Tang  Zilu <tangzilu@4paradigm.com>
2021-05-25 17:23:38 +0800

    add first

---
+++ idfpocr/src/main/java/com/hxocr/idfpocr/IdfpocrApplication.java
@@ -0,0 +1,13 @@
+package com.hxocr.idfpocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfpocrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdfpocrApplication.class, args);
+    }
+
+}
