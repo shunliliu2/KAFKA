1e9bc94f4a64b5e79836abd835842d0289a2fd99
shunliliu2 <89114770+shunliliu2@users.noreply.github.com>
2022-10-20 11:48:39 +0800

    Delete duanxin2 directory

--- duanxin2/src/test/java/ttt.java
+++
@@ -1,7 +0,0 @@
-public class ttt {
-    public static void main(String[] args){
-        System.out.println("===========================");
-        System.out.println(System.getProperty("java.library.path"));
-        System.out.println("===========================");
-    }
-}
